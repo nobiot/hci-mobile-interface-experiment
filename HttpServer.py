6d93b2b026125488e7b72156854f965145841014
nobiot <noboru.ota@gmail.com>
2016-06-25 09:42:58 +0100

    Modified HttpServer.py to control the directory. No longer accepting from the app

--- HttpServer.py
+++ HttpServer.py
@@ -10,30 +10,31 @@ import cgi
 
 # Port on which server will run.
 PORT = 8080
- 
+# File path relative to the current directory
+# TODO file name should be dynamically determined.
+# Prhaps send a content from the app
+DIRECTORY = "results"
+EXTENSION = ".csv"
  
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
- 
+    
     def do_POST(self):
-        """ Handle POST Request"""
-        # Check if path is there.
-        if self.path:
+        # Get length of the data and read it.
+        length = self.headers['content-length']
+        data = self.rfile.read(int(length))
  
-            # Get length of the data and read it.
-            length = self.headers['content-length']
-            data = self.rfile.read(int(length))
+        # Write the data to a file in current dir.
+        filename = 'particpantN' + EXTENSION
+        path = os.path.join(os.getcwd(), DIRECTORY, filename)
+        with open(path, 'wb') as file:
+            file.write(data)
  
-            # Write the data to a file in current dir.
-            with open(os.getcwd() + self.path, 'wb') as file:
-                file.write(data)
- 
-            # Send success response.
-            self.send_header('Content-type', 'text/json')
-            self.send_response(200, 'OK')
-            self.end_headers()
+        # Send success response.
+        self.send_header('Content-type', 'text/json')
+        self.send_response(200, 'OK')
+        self.end_headers()
         http.server.SimpleHTTPRequestHandler.do_GET(self)  
  
-
 Handler = HTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
