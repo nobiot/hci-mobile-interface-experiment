e7d28f61885eaca3ac9d2d10a0a23cdab419c737
nobiot <noboru.ota@gmail.com>
2016-06-25 10:38:31 +0100

    Refactored experiment.js basd on linting

--- js/experiment.js
+++ js/experiment.js
@@ -2,45 +2,62 @@
 // array.includes(element) is not supported on all platforms (notably, IE and Edge do not).
 
 window.onload = function () {
+    'use strict';
     
-    var timeStart;
-    var tiemEnd;
-
-    var numberOfMenuItems = 8; // default number of menu items
-    var dictionary = ["Apple", "Orange", "Mango", "Açaí", "Ackee",
+    var timeStart,
+        timeEnd,
+        numberOfMenuItems = 8, // default number of menu items
+        dictionary = ["Apple", "Orange", "Mango", "Açaí", "Ackee",
                       "Banana", "Batuan", "Caimito", "Cantaloupe", "Chinese olive",
                       "Date", "Durian", "Galia melon", "Grumichama", "Guava"
-                    ]; // words taken from https://en.wikipedia.org/wiki/List_of_culinary_fruits
+                    ], // words taken from https://en.wikipedia.org/wiki/List_of_culinary_fruits
+        buttonStart = document.getElementById("buttonStart"),
+        menuItemTarget,
+        textTarget,
+        resultNumber = 0;
     
-
-    var buttonStart = document.getElementById("buttonStart");
-    var menuItemTarget;
-    var textTarget;
-    var resultNumber = 0;
-     
-    function endTimer() {
-            timeEnd = Date.now();
-            writeResult();
-            experimentReset();
+    function targetRemove() {
+        var element = document.getElementById("targetDisplay");
+        element.innerHTML = "";
     }
-    function menuCreate(n) {
-        var menuItems = [n];
-        var targetIndex = Math.floor((Math.random() * n )) //get a number from 0 to n-1 to be the index of the target
-        for (var i=0; i < n; i++){ // get n items 
-            var item = dictionary[Math.floor((Math.random() * dictionary.length))]; // generate random to length of the dictionary
-            
-            if (menuItems.includes(item) == false){ // check if the item is already in the chosen one
-                menuItems[i] = item;
-            }else{
-                i--; //if the item is already in the array, then the loop needs repeat the
-                     //same iteration
-                     //come back to this. very ugly solution
-            }
-        }
-        // Create <li> elements
+    
+    function experimentReset() {
         var ul = document.getElementById("navigationMenu");
-        
-// Framework7 
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+        targetRemove();
+    }
+
+    function writeResult() {
+        resultNumber += 1;
+        var resultDisplay = document.getElementById("resultDisplay"),
+            p = document.createElement("p"),
+            duration = timeEnd - timeStart;
+        p.innerHTML = "Result " + resultNumber + ": " + duration + " milliseconds";
+        resultDisplay.appendChild(p);
+    }
+
+    function endTimer() {
+        timeEnd = Date.now();
+        writeResult();
+        experimentReset();
+    }
+
+    function menuCreate(n) {
+        var menuItems = [n],
+            counter = 0,
+            targetIndex = Math.floor((Math.random() * n)); //get a number from 0 to n-1 to be the index of the target
+        while (counter < n) {
+            // Randomly pick an item from the dictionary of any length.
+            var item = dictionary[Math.floor((Math.random() * dictionary.length))];
+            // Add the item only when it is already in the menuItems array.
+            if (menuItems.includes(item) === false) {
+                menuItems[counter] = item;
+                counter += 1;
+            }//Repeat and pick an item again if it is already in the array.
+        }
+// Construction the unordered list (ul) for the menu based on Framework7's structure as follows:
 //                   <li>
 //                    <a class="item-link close-panel" href="forms.html">
 //                        <div class="item-content">
@@ -53,7 +70,8 @@ window.onload = function () {
 //                        </div>
 //                    </a>
 //                    </li>
-//        
+// 
+        var ul = document.getElementById("navigationMenu");
         for (var i=0; i<n ;i++) {
             var li = document.createElement("li");
             var a  = document.createElement("a");
@@ -83,37 +101,17 @@ window.onload = function () {
         menuItemTarget = document.getElementById("menuItemTarget");
         menuItemTarget.onclick = endTimer;
     }
-    
+
     function targetDisplay() {
         var element = document.getElementById("targetDisplay");
         element.innerHTML = textTarget;
     }
-    function targetRemove() {
-        var element = document.getElementById("targetDisplay");
-        element.innerHTML = "";
-    }
     
-    function experimentReset() {
-        var ul = document.getElementById("navigationMenu");
-        while(ul.firstChild) {
-            ul.removeChild(ul.firstChild);
-        }
-        targetRemove();
-    }
-    
-    function writeResult() {
-        resultNumber += 1;
-        var resultDisplay = document.getElementById("resultDisplay");
-        var p = document.createElement("p")
-        var duration = timeEnd - timeStart;
-        p.innerHTML = "Result " + resultNumber + ": " + duration + " milliseconds";
-        resultDisplay.appendChild(p);
-    }
-
     buttonStart.onclick = function experimentStart() {
         experimentReset();
         menuCreate(numberOfMenuItems);
-        targetDisplay()
+        targetDisplay();
         timeStart = Date.now();
     }
+
 }
