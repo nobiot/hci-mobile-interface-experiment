75428cd9ad0b23bf1b5936dee31df5afe0edbf59
nobiot <noboru.ota@gmail.com>
2016-06-28 10:37:18 +0100

    Ported HttpServer.py to python2 code (HttpServerPy2.py)

---
+++ HttpServerPy2.py
@@ -0,0 +1,41 @@
+## Porting HttpServer.py to Python 2
+
+
+
+import os
+import SimpleHTTPServer
+import SocketServer
+
+
+# Port on which server will run.
+PORT = 8080
+# File path relative to the current directory
+# TODO file name should be dynamically determined.
+# Prhaps send a content from the app
+DIRECTORY = "results"
+EXTENSION = ".csv"
+
+class HTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    
+    def do_POST(self):
+        # Get length of the data and read it.
+        length = self.headers['content-length']
+        data = self.rfile.read(int(length)) #TODO: Now I am sending JSON. Parse it to create unique file names
+        
+# Write the data to a file in current dir.
+        filename = 'particpantN' + EXTENSION
+        path = os.path.join(os.getcwd(), DIRECTORY, filename)
+        with open(path, 'wb') as file:
+            file.write(data)  
+            
+        # Send success response.
+        self.send_header('Content-type', 'text/json')
+        self.send_response(200, 'OK')
+        self.end_headers()
+        
+Handler = HTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
+        
+        
