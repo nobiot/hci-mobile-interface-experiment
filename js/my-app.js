278f88631e8b2a7823979b97d0a09c4974745c48
nobiot <noboru.ota@gmail.com>
2016-07-15 09:14:49 +0100

    Navigation works

--- js/my-app.js
+++ js/my-app.js
@@ -6,11 +6,6 @@ var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+    domCache: true
 });
 
-myApp.onPageInit('pattern-1-go', function(page) {
-                 var buttonStart = document.getElementById("buttonStart").onclick = NOBexperiment.getFuncStart;
-                 //probably listener is better TODO    
-                 })
